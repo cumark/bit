48c1c749830f87af1b5e1bba720b606e05247703
cumark <34766761+cumark@users.noreply.github.com>
2022-04-14 02:36:22 +0800

    Update te.py
    {"username":"2@liniuniu.top","email":"2@liniuniu.top","password":"cumark","recaptchaToken":"03AGdBq27vBuEUuFtKSjy-VRa1nn__pyfNeg1Ze7RA21e0zzMjjSbs_k8igyxeZDfsQTgGspWLTyVU_fUuO6qIvX1-Wtmm30WGFJ0TV1rVOTSzfm9gxtd0g2G-_6LVcjgLCtlNhzk1jUFcRWvgMAXi4xF6Z0vF7X3pbTqIkgBw4WneeaTm54jOhYeOgY-7FMHEztVM0qEmG-la0rU8S7c5HEFpjNj1Ee-vUnvxvyX0MKJTXjWMWUyiT8Tb8qn9yb4TWcKAalDb0242IZ7RdoU_xoc-KUyr0roRS98JmgcYCApa-8SN07dEyfGSEFrDlD-EOuRYhLjJsaW_LJlo04YBJnYWEZnBsZxFBvFXDkheA0ct-k2ZSM1AD6YJpcunAQzP1o0chtwRBqV2l7xFMq6rW_KaOxaXk_KMQTPQZSNFIDRJWESoD-Q_1_AMlBO_ylJmIoR5gqephQfc","custom_fields":{},"avatar":"https://cdn.mycourse.app/v2.0.19/images/empty-avatar.jpg","account":{"type":"learnworlds"},"eu_customer":false}

--- te.py
+++ te.py
@@ -31,9 +31,7 @@ def cr(i):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36',}
      #'Cookie': 'slim_session=7otEozF8skmAK8pQkdDnGElyyU95Nsw90BBp6Vq7; _ga=GA1.2.480091820.1649407063; _gid=GA1.2.71216230.1649407063; _gat_gtag_UA_68126873_1=1'
     data = {"username": cnt+"@liniuniu.top", "email": cnt + "@liniuniu.top",
-            "password": "cumark", "custom_fields": {},
-            "avatar": "https://cdn.mycourse.app/v2.0.9/images/empty-avatar.jpg"
-               , "account": {"type": "learnworlds"}, "eu_customer": 'false'}
+            "password": "cumark", "custom_fields":{},"avatar":"https://cdn.mycourse.app/v2.0.19/images/empty-avatar.jpg","account":{"type":"learnworlds"},"eu_customer":false}
     print(data['username'])
     while 1:
         conment = requests.post('https://salad.academy/api/users', headers=headers,json=data,proxies=proxies)
